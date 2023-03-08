4cfe4b160ff2661cbae4b00e275771b5d58c8511
Álvaro Itu <contato@alvaroitu.com>
2023-03-07 21:17:47 -0300

    first commit

---
+++ Aula01_layout/app/src/main/java/com/alvaroitu/aula01_layout/MainActivity.java
@@ -0,0 +1,38 @@
+package com.alvaroitu.aula01_layout;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button botaoEnviar;
+    Button botaoLimpar;
+    TextView texto;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        botaoEnviar = findViewById(R.id.buttonEnviar);
+        botaoLimpar = findViewById(R.id.buttonLimpar);
+        texto = findViewById(R.id.textView);
+
+        botaoEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                texto.setText("Olá Mundo...");
+            }
+        });
+
+        botaoLimpar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                texto.setText("");
+            }
+        });
+    }
+}
